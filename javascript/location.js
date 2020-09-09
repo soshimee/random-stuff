58d206850081fbc10b9db9760c86b76ef5809cfb
최진이 <choejin-i@choejin-iui-MacBookAir.local>
2020-09-09 21:59:41 +0900

    placeholder & to function

--- javascript/location.js
+++ javascript/location.js
@@ -1,10 +1,12 @@
-var isFloat = /(^\d+\.\d+$)|(^\d+$)/
+function isFloat(str) {
+	return /(^\d+\.\d+$)|(^\d+$)/.test(str);
+}
 
 $("#search").click(() => {
 	var lng = $("#lngText").val();
 	var lat = $("#latText").val();
 
-	if (!isFloat.test(lng) || !isFloat.test(lat)) {
+	if (!isFloat(lng) || !isFloat(lat)) {
 		alert("Enter a number.");
 	} else {
 		open(`https://google.com/maps/dir/${lng},${lat}`);
