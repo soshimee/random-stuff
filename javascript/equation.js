f85d083a993181457330650e45270af69223b9d3
최진이 <choejin-i@choejin-iui-MacBookAir.local>
2020-09-11 21:40:28 +0900

    ​

--- javascript/equation.js
+++ javascript/equation.js
@@ -13,7 +13,6 @@ $("#equationText").on("input", () => {
 			$(".rendered__equation").html("<img class=\"rendered__image\">")
 			$(".rendered__image").attr("src", `https://latex.codecogs.com/svg.latex?\\inline&space;${equation}`)
 		} else {
-			// If there is no if statement, LaTeX equation may be show as plain text while typing.
 			$(".rendered__equation").html(`\\(${equation}\\)`);
 			MathJax.typeset();
 		}
