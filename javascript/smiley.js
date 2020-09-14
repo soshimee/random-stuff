33f6c359c79791021b8404a96b24bc7adf7ed706
최진이 <choejin-i@choejin-iui-MacBookAir.local>
2020-09-14 21:43:49 +0900

    ​

--- javascript/smiley.js
+++ javascript/smiley.js
@@ -1,8 +1,4 @@
 $("#form").submit((e) => {
 	e.preventDefault();
-	if (/^[:;][-~]?[\)D]$/.test($("#text").val())) {
-		$("output").html("True");
-	} else {
-		$("output").html("False");
-	}
+	(/^[:;][-~]?[\)D]$/.test($("#text").val())) ? $("output").html("True") : $("output").html("False");
 });
